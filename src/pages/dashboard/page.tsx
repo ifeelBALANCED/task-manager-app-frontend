0387094eae0a5ed4cd9102b83219828eddd1f35a
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-09-01 02:03:35 +0300

    fix: no data when tasks is empty + filters select

--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { AreaChart, PieChart } from '@mantine/charts'
-import { Badge, Card, Grid, Group, Loader, Select, Title } from '@mantine/core'
+import { Badge, Card, Grid, Group, Loader, Select, Text, Title } from '@mantine/core'
+import { IconChartLine, IconChartPie } from '@tabler/icons-react'
 import { useGate, useUnit } from 'effector-react'
 import {
   $areaChartData,
@@ -34,7 +35,11 @@ export const DashboardPage = () => {
             placeholder="Select date"
             data={dateOptions}
             value={selectedDate}
-            onChange={setSelectedDate}
+            onChange={(date) => {
+              if (date !== null) {
+                setSelectedDate(date)
+              }
+            }}
           />
         </Group>
       </div>
@@ -50,18 +55,27 @@ export const DashboardPage = () => {
               <Title order={4} className="mb-4">
                 Task Status Over Time
               </Title>
-              <AreaChart
-                h={300}
-                data={areaChartData}
-                dataKey="date"
-                series={[
-                  { name: 'TODO', color: 'indigo.6' },
-                  { name: 'IN_PROGRESS', color: 'blue.4' },
-                  { name: 'DONE', color: 'green.4' },
-                  { name: 'POSTPONED', color: 'red.4' },
-                ]}
-                curveType="linear"
-              />
+              {areaChartData.length === 0 ? (
+                <div className="flex items-end gap-x-2">
+                  <IconChartLine size={30} color="gray" />
+                  <Text mt="sm" c="dimmed">
+                    No data available for the selected date range.
+                  </Text>
+                </div>
+              ) : (
+                <AreaChart
+                  h={300}
+                  data={areaChartData}
+                  dataKey="date"
+                  series={[
+                    { name: 'TODO', color: 'indigo.6' },
+                    { name: 'IN_PROGRESS', color: 'blue.4' },
+                    { name: 'DONE', color: 'green.4' },
+                    { name: 'POSTPONED', color: 'red.4' },
+                  ]}
+                  curveType="linear"
+                />
+              )}
             </Card>
           </Grid.Col>
           <Grid.Col span={12}>
@@ -69,7 +83,22 @@ export const DashboardPage = () => {
               <Title order={4} className="mb-4">
                 Task Status Overview
               </Title>
-              <PieChart data={pieChartData} mx="auto" withLabels labelsType="percent" withTooltip />
+              {pieChartData.map((pie) => pie.value).every((value) => value === 0) ? (
+                <div className="flex items-end gap-x-2">
+                  <IconChartPie size={30} color="gray" />
+                  <Text mt="sm" c="dimmed">
+                    No data available for task status overview.
+                  </Text>
+                </div>
+              ) : (
+                <PieChart
+                  data={pieChartData}
+                  mx="auto"
+                  withLabels
+                  labelsType="percent"
+                  withTooltip
+                />
+              )}
             </Card>
           </Grid.Col>
         </Grid>
