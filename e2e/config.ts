868ff778b00150d7bee2592913ebcaf1ec43bee5
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-09-02 14:24:49 +0300

    test: correct the work with env ci settings

--- e2e/config.ts
+++ e2e/config.ts
@@ -3,7 +3,9 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BASE_URL = `http://localhost:5173`
+const CI_ENABLED = process.env.VITE_CI === 'true'
+const PORT = CI_ENABLED ? 4173 : 5173
+export const BASE_URL = `http://localhost:${PORT}`
 
 export const apiTest = axios.create({
   baseURL: BASE_URL,
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig, devices } from '@playwright/test'
+import * as dotenv from 'dotenv'
 
-const PORT = 5173
+dotenv.config()
+
+const CI_ENABLED = process.env.VITE_CI === 'true'
+const PORT = CI_ENABLED ? 4173 : 5173
 
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.VITE_CI,
-  retries: process.env.VITE_CI ? 2 : 0,
-  workers: process.env.VITE_CI ? 5 : undefined,
+  forbidOnly: CI_ENABLED,
+  retries: CI_ENABLED ? 2 : 0,
+  workers: CI_ENABLED ? 5 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
@@ -25,7 +29,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: process.env.VITE_CI ? 'pnpm run build && pnpm run preview' : 'pnpm run dev',
+    command: CI_ENABLED ? 'pnpm run build && pnpm run preview' : 'pnpm run dev',
     url: `http://localhost:${PORT}`,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
