5cbc2bbc65dcf7e60259b0128c30ed672c284897
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-09-02 12:06:04 +0300

    ci: provide the test base url env for deployed tests

--- e2e/config.ts
+++ e2e/config.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import 'dotenv/config'
 
-export const BASE_URL = process.env.VITE_BASE_URL
+export const BASE_URL =
+  process.env.VITE_BASE_URL || `http://localhost:${process.env.VITE_CI ? 4173 : 5173}`
 
 export const apiTest = axios.create({
   baseURL: BASE_URL,
